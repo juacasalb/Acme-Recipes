c240253aa0559464746e97fadeba2b96505bc1db
juacasalb <juacasalb@alum.us.es>
2022-07-12 14:28:56 +0200

    D2-04 | D2-05 | D2-06 Done

---
+++ src/main/java/acme/entities/Item.java
@@ -0,0 +1,53 @@
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Item extends AbstractEntity{
+
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank
+	@Length(min = 1, max = 101)
+	protected String name;
+	
+	@NotNull
+	protected ItemType type;
+	
+	@NotBlank
+	@Column(unique=true)
+	@Pattern(regexp="^([A-Z]{2}:)?[A-Z]{3}-[0-9]{3}$")
+	protected String code;
+	
+	@NotBlank
+	@Length(min = 1, max = 256)
+	protected String description;
+	
+	@NotNull
+	@Valid
+	protected Money retailPrice;
+	
+	@URL
+	protected String link;
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=true)
+	protected Recipe recipe;
+}
---
+++ src/main/java/acme/entities/Recipe.java
@@ -0,0 +1,42 @@
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Recipe extends AbstractEntity{
+
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank
+	@Column(unique=true)
+	@Pattern(regexp="^([A-Z]{2}:)?[A-Z]{3}-[0-9]{3}$")
+	protected String code;
+	
+	@NotBlank
+	@Length(min = 1, max = 101)
+	protected String heading;
+	
+	@NotBlank
+	@Length(min = 1, max = 256)
+	protected String description;
+	
+	@NotBlank
+	@Length(min = 1, max = 256)
+	protected String preparationNotes;
+	
+	@URL
+	protected String link;
+	
+}
