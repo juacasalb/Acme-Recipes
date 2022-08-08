912b283def20d824afd83ab6048974a7c8506aa3
vicgrabru <vgravanbru@gmail.com>
2022-08-08 17:27:06 +0200

    D4-8: hecho

---
+++ src/main/java/acme/features/chef/memoranda/ChefMemorandaCreateService.java
@@ -0,0 +1,138 @@
+package acme.features.chef.memoranda;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Memorandum;
+import acme.entities.fineDish.FineDish;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Errors;
+import acme.framework.controllers.Request;
+import acme.framework.services.AbstractCreateService;
+import acme.roles.Chef;
+
+@Service
+public class ChefMemorandaCreateService implements AbstractCreateService<Chef, Memorandum> {
+	
+	@Autowired
+	protected ChefMemorandaRepository repository;
+	
+	@Override
+	public boolean authorise(final Request<Memorandum> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<Memorandum> request, final Memorandum entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		String fineDishCode, seqNum;
+		FineDish fd;
+		Long memAmount;
+		
+		fineDishCode = request.getModel().getString("fineDishCode");
+		fd = this.repository.getFineDishByCode(fineDishCode);
+		
+		memAmount = this.repository.countMemorandum() + 1;
+		seqNum = this.getSeqNumFromAmountAndFineDishCode(fineDishCode, memAmount);
+		
+		entity.setFineDish(fd);
+		entity.setAutomaticSeqNum(seqNum);
+		System.out.println(seqNum);
+		
+		request.bind(entity, errors, "instantiationMoment", "report", "link");
+		
+		System.out.println("===================================");
+		System.out.println("Attributes de la entity:");
+		
+		System.out.println("automaticSeqNum: " + entity.getAutomaticSeqNum());
+		System.out.println("fineDish: " + entity.getFineDish().toString());
+		System.out.println("instantiationMoment: " + entity.getInstantiationMoment().toString());
+		System.out.println("report: " + entity.getReport());
+		System.out.println("link: " + entity.getLink());
+		
+		
+		System.out.println("===================================");
+		
+	}
+
+	@Override
+	public void unbind(final Request<Memorandum> request, final Memorandum entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		
+		request.unbind(entity, model,"instantiationMoment", "report", "link");
+	}
+
+	@Override
+	public Memorandum instantiate(final Request<Memorandum> request) {
+		assert request != null;
+		Memorandum result;
+		
+		result = new Memorandum();
+		
+		result.setInstantiationMoment(Date.valueOf(LocalDate.now()));
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<Memorandum> request, final Memorandum entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		final String code;
+		final FineDish fd;
+		
+		//Comprueba que se ha marcado la casilla de confirmacion
+		if(!errors.hasErrors("confirmation")) {
+			errors.state(request, request.getModel().getBoolean("confirmation"), "confirmation", "chef.memoranda.confirmation.required");
+		}
+		//Comprueba que hay un FineDish con el codigo especificado
+		if(!errors.hasErrors("fineDishCode")) {
+			code = request.getModel().getString("fineDishCode");
+			fd = this.repository.getFineDishByCode(code);
+			errors.state(request, fd != null, "fineDishCode", "chef.memoranda.fine-dish-code.fine-dish-doesnt-exist");
+		}
+		
+		
+	}
+
+	@Override
+	public void create(final Request<Memorandum> request, final Memorandum entity) {
+		assert request != null;
+		assert entity != null;
+		
+		this.repository.save(entity);
+	}
+	
+	
+	private String getSeqNumFromAmountAndFineDishCode(final String code, final Long amount) {
+		String aux, result;
+		aux = Long.toString(amount);
+		
+		if (amount>9999) {
+			result = aux.substring(aux.length()-4);
+		}
+		else if (amount<1000){
+			result = String.join("",Collections.nCopies((4-aux.length()),"0")) + aux;
+		}
+		else {
+			result = aux;
+		}
+		
+		return code + ":" + result;
+	}
+	
+	
+	
+	
+}
