1a3108d78278533f225a4e7dea46b2d4671f8586
vicgrabru <vgravanbru@gmail.com>
2022-08-31 19:41:21 +0200

    En proceso

--- src/main/java/acme/entities/Item.java
+++ src/main/java/acme/entities/Item.java
@@ -3,6 +3,7 @@ package acme.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -59,4 +60,8 @@ public class Item extends AbstractEntity{
 	@ManyToOne(optional=false)
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	protected Chef chef;
+	
+	@Valid
+	@OneToOne(optional = false, mappedBy="pimpam")
+	protected Item item;
 }
---
+++ src/main/java/acme/entities/Pimpam.java
@@ -0,0 +1,93 @@
+package acme.entities;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Pimpam extends AbstractEntity{
+
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank
+	@Column(unique=true)
+	@Pattern(regexp="[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")
+	/* yy-mm-dd (fecha de instantiationMoment)
+	 * yy: [0-9]{2} (caracter entre 0 y 9 inclusive, 2 veces)
+	 * mm: (0[1-9]|1[0-2])
+	 * 		opcion 1:
+	 * 			0[1-9] (caracter 0 seguido de un caracter entre 1 y 9 inclusive)
+	 * 		opcion 2:
+	 * 			1[0-2] (caracter 1 seguido de un caracter entre 0 y 2 inclusive)
+	 * dd: (0[1-9]|[1-2][0-9]|3[0-1])
+	 * 		opcion 1: 
+	 * 			0[1-9] (caracter 0 seguido de un caracter entre 1 y 9 inclusive)
+	 * 		opcion 2:
+	 * 			[1-2][0-9] (caracter entre 1 y 2 inclusive seguido de un caracter entre 0 y 9 inclusive)
+	 * 		opcion 3:
+	 * 			3[0-1] (caracter 3 seguido de caracter entre 0 y 1 inclusive)
+	 * */
+	protected String code;
+	
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date instantationMoment;
+	
+	@NotBlank
+	@Length(min = 1, max = 101)
+	protected String title;
+	
+	@NotBlank
+	@Length(min = 1, max = 256)
+	protected String description;
+	
+	@NotNull
+	@Valid
+	protected Money budget;
+	
+	@URL
+	protected String link;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date startPeriod;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date endPeriod;
+	
+	@Valid
+	@OneToOne(optional = false, mappedBy="pimpam")
+	protected Item item;
+	
+//	@Valid
+//	@Temporal(TemporalType.TIMESTAMP)
+//	protected Date period;
+//	
+//	@Transient
+//	protected void setPeriod() {
+//		this.period = new Date(this.endPeriod.getTime() - this.instantationMoment.getTime());
+//	}
+	
+}
