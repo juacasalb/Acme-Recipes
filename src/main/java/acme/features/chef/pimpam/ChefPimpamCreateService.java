8cad8890b1cd40977c55ad2cb6b7ca97dec3e144
Miguel <miggavmar@alum.us.es>
2022-09-06 19:38:16 +0200

    validate currency in create & update services

--- src/main/java/acme/features/chef/pimpam/ChefPimpamCreateService.java
+++ src/main/java/acme/features/chef/pimpam/ChefPimpamCreateService.java
@@ -1,13 +1,16 @@
 package acme.features.chef.pimpam;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.Item;
 import acme.entities.Pimpam;
+import acme.features.administrator.systemConfiguration.AdministratorSystemConfigurationRepository;
 import acme.features.chef.item.ChefItemRepository;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Errors;
@@ -23,6 +26,9 @@ public class ChefPimpamCreateService implements AbstractCreateService<Chef, Pimp
 	
 	@Autowired
 	protected ChefItemRepository itemRepository;
+	
+	@Autowired
+	protected AdministratorSystemConfigurationRepository currencyRepository;
 	
 	@Override
 	public boolean authorise(final Request<Pimpam> request) {
@@ -89,6 +95,14 @@ public class ChefPimpamCreateService implements AbstractCreateService<Chef, Pimp
 		assert entity != null;
 		assert errors != null;
 	
+		
+		if (!errors.hasErrors("budget")) {
+			
+			
+			final boolean availableCurrency = this.validateAvailableCurrency(entity.getBudget().toString().substring(2,5));
+			errors.state(request, availableCurrency, "budget", "chef.pimpam.form.error.currency-not-available");
+
+		}
 	}
 
 	@Override
@@ -101,6 +115,14 @@ public class ChefPimpamCreateService implements AbstractCreateService<Chef, Pimp
 		
 		this.repository.save(entity);
 		
+	}
+	
+	public boolean validateAvailableCurrency(final String currency) {
+
+		final String currencies = this.currencyRepository.findAvailableCurrencies();
+		final List<Object> listOfAvailableCurrencies = Arrays.asList((Object[]) currencies.split(","));
+
+		return listOfAvailableCurrencies.contains(currency);
 	}
 
 	
--- src/main/java/acme/features/chef/pimpam/ChefPimpamUpdateService.java
+++ src/main/java/acme/features/chef/pimpam/ChefPimpamUpdateService.java
@@ -1,10 +1,14 @@
 package acme.features.chef.pimpam;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.Item;
 import acme.entities.Pimpam;
+import acme.features.administrator.systemConfiguration.AdministratorSystemConfigurationRepository;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Errors;
 import acme.framework.controllers.Request;
@@ -16,6 +20,9 @@ public class ChefPimpamUpdateService implements AbstractUpdateService<Chef, Pimp
 
 	@Autowired
 	protected ChefPimpamRepository repository;
+	
+	@Autowired
+	protected AdministratorSystemConfigurationRepository currencyRepository;
 	
 	@Override
 	public boolean authorise(final Request<Pimpam> request) {
@@ -58,7 +65,7 @@ public class ChefPimpamUpdateService implements AbstractUpdateService<Chef, Pimp
 		assert entity != null;
 		assert model != null;
 		
-		request.unbind(entity, model, "title", "code", "description", "budget", "link", "finishingDate", "instantationMoment", "item.name", "published");
+		request.unbind(entity, model, "title", "code", "description", "budget", "link", "finishingDate", "instantationMoment", "item.name");
 		
 	}
 
@@ -83,6 +90,13 @@ public class ChefPimpamUpdateService implements AbstractUpdateService<Chef, Pimp
 		assert entity != null;
 		assert errors != null;
 		
+		if (!errors.hasErrors("budget")) {
+			
+			
+			final boolean availableCurrency = this.validateAvailableCurrency(entity.getBudget().toString().substring(2,5));
+			errors.state(request, availableCurrency, "budget", "chef.pimpam.form.error.currency-not-available");
+
+		}
 	}
 
 	@Override
@@ -94,6 +108,19 @@ public class ChefPimpamUpdateService implements AbstractUpdateService<Chef, Pimp
 		this.repository.save(entity);
 		
 	}
+	
 
+	public boolean validateAvailableCurrency(final String currency) {
+
+		final String currencies = this.currencyRepository.findAvailableCurrencies();
+		final List<Object> listOfAvailableCurrencies = Arrays.asList((Object[]) currencies.split(","));
+
+		return listOfAvailableCurrencies.contains(currency);
+	}
+
+
+	
+	
+	
 	
 }
